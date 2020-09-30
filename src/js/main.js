e67e5faf79d10537863321bb4fc5b88e8ea0a51a
serii <seriiburduja@gmail.com>
2020-09-30 17:23:37 +0300

    delete src slick

--- src/js/main.js
+++
@@ -1,6 +0,0 @@
-import sum from "./test";
-
-document.addEventListener('DOMContentLoaded', function () {
-	console.log(sum(4, 87));
-	console.log(sum(8));
-});
--- src/js/test.js
+++
@@ -1,4 +0,0 @@
-export default function sum (a, b = 4) {
-	[1,3,4].forEach(item => console.log(item))
-	return a + b;
-}
