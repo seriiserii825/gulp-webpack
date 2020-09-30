6fed9d55cee59a5978122f7088376d8a0e7359fa
serii <seriiburduja@gmail.com>
2020-09-30 15:08:51 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import sum from "./test";
+
+document.addEventListener('DOMContentLoaded', function () {
+	console.log(sum(4, 87));
+	console.log(sum(8));
+});
---
+++ src/js/test.js
@@ -0,0 +1,4 @@
+export default function sum (a, b = 4) {
+	[1,3,4].forEach(item => console.log(item))
+	return a + b;
+}
