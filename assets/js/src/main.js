c8470288a6e9e6f0fe7c07ebb9d13c0cb8257e0b
serii <seriiburduja@gmail.com>
2021-03-04 18:34:23 +0200

    main.js examples

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -1,12 +1,12 @@
 import gsapAnimation from "./modules/animation";
 import touchMenu from "./modules/touch-menu";
 
-document.addEventListener('DOMContentLoaded', function () {
-	if (window.document.documentMode) {
-		// alert('Internet Explorer');
-		console.log('internet expolorer');
-	} else {
-		gsapAnimation();
-		touchMenu();
-	}
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.document.documentMode) {
+    // alert('Internet Explorer');
+    console.log("internet expolorer");
+  } else {
+    // gsapAnimation();
+    // touchMenu();
+  }
 });
