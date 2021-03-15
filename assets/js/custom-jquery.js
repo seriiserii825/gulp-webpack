105b66edb081eaae983fea8c87ecb55fd43983e1
serii <seriiburduja@gmail.com>
2021-03-15 12:39:22 +0200

    padding right for bbody

--- assets/js/custom-jquery.js
+++ assets/js/custom-jquery.js
@@ -1,22 +1,22 @@
 jQuery(document).ready(function ($) {
-  const mainMenu = $('#js-main-menu');
+  const mainMenu = $("#js-main-menu");
   let closeMenu = function () {
-    $('#js-sandwitch-wrap').on('click', function () {
-      if (mainMenu.hasClass('fixed')) {
-        mainMenu.removeClass('fixed');
-        $('body').css({
-          overflow: '',
-          paddingRight: 'initial'
+    $("#js-sandwitch-wrap").on("click", function () {
+      if (mainMenu.hasClass("fixed")) {
+        mainMenu.removeClass("fixed");
+        $("body").css({
+          overflow: "",
+          paddingRight: "initial",
         });
       } else {
-        $('body').css({
-          overflow: 'hidden',
-          paddingRight: '12px'
+        $("body").css({
+          overflow: "hidden",
+          paddingRight: document.querySelector(".wrapper").offsetWidth + "px",
         });
-        mainMenu.addClass('fixed');
+        mainMenu.addClass("fixed");
       }
-      $(this).toggleClass('sandwitch--active');
-      $(this).closest('.sandwitch-wrapper').toggleClass('sandwitch--active');
+      $(this).toggleClass("sandwitch--active");
+      $(this).closest(".sandwitch-wrapper").toggleClass("sandwitch--active");
     });
   };
   closeMenu();
